46ae19acea19f726fe652955e2510c29fbd779cf
Chandler Mathews <ctmathews2@usfca.edu>
2022-09-19 22:29:07 -0500

    call riot api

--- backend/App.js
+++ backend/App.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const Quote = require('inspirational-quotes');
 const app = express();
+const axios = require('axios');
+// const got = require('got');
+// const { pipeline } = require('stream');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -9,8 +12,33 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", function(req, res) {
-  res.send(Quote.getQuote());
+  // console.log('HERE', req.query);
+  axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.query.answer}?api_key=`)
+  .then(function(response) {
+    // console.log(response.data);
+    res.send(response.data);
+  }).catch(function(err) {
+    console.log(err);
+  });
+
 });
+
+// router.get('/', function(req, res) {
+//   const dataStream = got.stream({
+//       uri: 'http://www.giantbomb.com/api/search',
+//       qs: {
+//         api_key: '123456',
+//         query: 'World of Warcraft: Legion'
+//       }
+//   });
+//   pipeline(dataStream, res, (err) => {
+//       if (err) {
+//           console.log(err);
+//           res.sendStatus(500);
+//       }
+//   });
+// });
+
 
 let port = process.env.PORT;
 if(port == null || port == "") {
@@ -20,3 +48,5 @@ if(port == null || port == "") {
 app.listen(port, function() {
  console.log("Server started successfully");
 });
+
+// module.exports = router;
--- frontend/src/quotes.jsx
+++ frontend/src/quotes.jsx
@@ -1,22 +1,26 @@
 import React, {useState} from "react";
 import axios from "axios";
 function Quotes() {
-  const [text, setText] = useState("");
-  const [author, setAuthor] = useState("");
-function getQuote() {
-    axios.get("http://localhost:5000/",  { crossdomain: true }).then(response => {
-      setText(response.data.text);
-      setAuthor(response.data.author);
-    });
-  }
-return (
-    <div>
-      <button onClick={getQuote}>
-        Generate Quote
-      </button>
-      <h1>{text}</h1>
-      <h3>{"-" + author}</h3>
-    </div>
-  )
+
+    const [text, setText] = useState("");
+    // const [author, setAuthor] = useState("");
+
+    function getQuote() {
+        axios.get("http://localhost:5000/",  { params: { answer: 'ysoseriious' } }).then(response => {
+            setText(response.data.name);
+            // setAuthor(response.data.author);
+            console.log(response.data.name);
+        });
+    }
+
+    return (
+        <div>
+            <input type="text" id="nameInput"></input>
+            <button onClick={getQuote}>
+                Generate Level
+            </button>
+            <h1>{text}</h1>
+        </div>
+    )
 }
 export default Quotes;
