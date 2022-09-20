ad09060a582315c1069a2f3bb8cb3e85ebe93a17
Chandler Mathews <ctmathews2@usfca.edu>
2022-09-19 19:48:52 -0500

    Added temp backend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Quotes from "./quotes";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Quotes />
+      </header>
+    </div>
+  );
+}
+export default App;
